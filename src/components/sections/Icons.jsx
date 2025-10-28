5311f2a3bfeeb2e91b78c3cde66f3171959680b0
Pixelito1 <diego842010asd@gmail.com>
2025-10-28 12:11:15 -0500

    add Angular

--- src/components/sections/Icons.jsx
+++ src/components/sections/Icons.jsx
@@ -5,21 +5,25 @@ import {
   RiTailwindCssFill,
 } from "react-icons/ri";
 import { IoLogoCss3 } from "react-icons/io";
-import { SiCsharp } from "react-icons/si";
+import { SiCsharp, SiAngular } from "react-icons/si"; // 👈 Importamos Angular
 
 const classes = "w-5 h-5 md:w-10 md:h-10";
-const html = <RiHtml5Fill className={`${classes}  text-orange-500`} />;
-const css = <IoLogoCss3 className={`${classes}  text-blue-700`} />;
+
+const html = <RiHtml5Fill className={`${classes} text-orange-500`} />;
+const css = <IoLogoCss3 className={`${classes} text-blue-700`} />;
 const js = <RiJavascriptFill className={`${classes} text-yellow-400`} />;
 const react = <RiReactjsFill className={`${classes} text-cyan-500`} />;
+const angular = <SiAngular className={`${classes} text-red-600`} />; 
 const tw = <RiTailwindCssFill className={`${classes} text-cyan-400`} />;
 const csharp = <SiCsharp className={`${classes} text-indigo-700`} />;
+
 
 export const logos = [
   { logo: html, text: "Html" },
   { logo: css, text: "Css" },
   { logo: js, text: "JavaScript" },
   { logo: react, text: "React" },
+  { logo: angular, text: "Angular" }, 
   { logo: tw, text: "TailWind" },
   { logo: csharp, text: "C Sharp" },
 ];
